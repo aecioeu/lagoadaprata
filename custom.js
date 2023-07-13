ee4324967834f3100234cd76c209fe9c9f3bb707
aecio <aeroimports2016@gmail.com>
2023-07-13 16:35:38 -0300

    main

--- custom.js
+++ custom.js
@@ -2,16 +2,32 @@
 
 $(document).ready(function () {
     //urls em que sera supimida a busca
-    var urls = [
-        'portal/editais/1/5/0/0/0/51/0/0/0/',
-        ''];
-    if (urls.some(function(v) { return window.location.href.indexOf(v) > -1 })) {
+    var urls = [{ name: "Justificativa de Ausência",
+          url : 'portal/editais/1/5/0/0/0/51/0/0/0/'
+        },{
+            name:'Termos de Fomento, Colaboração e Acordo de Cooperação',
+            url:'portal/editais/1/5/0/0/0/54/0/0/0/'
+        }
+    ]
+        
 
-       $('.ed_area_busca').remove()
-       $('.sw_titulo_pagina sw_ubuntu_light').text('MROSC - Parcerias')
-       $('.sw_area_links_navegacao').html(`<a href="/"><div class="sw_navegacao"><i class="swfa fas fa-home" aria-hidden="true"></i><span class="sw_lato">Principal</span></div></a>
-       <a href="${window.location.href}"><div class="sw_navegacao"><i class="swfa fas fa-home" aria-hidden="true"></i><span class="sw_lato">MROSC - Parcerias</span></div></a>`)
-    }
+        urls.forEach(function(key) {
+           console.log(key)
+
+           if (window.location.href.indexOf(key.url) > -1) {
+            
+            $('.ed_area_busca').remove()
+            $('.sw_titulo_pagina').html(key.name)
+            $('.sw_area_links_navegacao').html(`
+            <a href="/"><div class="sw_navegacao"><i class="swfa fas fa-home" aria-hidden="true"></i><span class="sw_lato">Principal</span></div></a>
+            <a href="https://lagoadaprata.mg.gov.br/parcerias"><div class="sw_navegacao"><i class="swfa fas fa-home" aria-hidden="true"></i><span class="sw_lato">MROSC - Parcerias</span></div></a>
+            <a href="${window.location.href}"><div class="sw_navegacao"><i class="swfa fas fa-home" aria-hidden="true"></i><span class="sw_lato">${key.name}</span></div></a>`)
+         }
+
+
+        });
+
+
 
 
 });
