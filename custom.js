d83f14fd96ab5aa9623e78812b95810b6417bb94
aecio <aeroimports2016@gmail.com>
2023-07-13 15:46:38 -0300

    main

---
+++ custom.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready(function () {
+
+    //urls em que sera supimida a busca
+    var urls = [
+        'portal/editais/1/5/0/0/0/51/0/0/0/',
+        ''];
+    if (urls.some(function(v) { return window.location.href.indexOf(v) > -1 })) {
+
+       $('.ed_area_busca').remove()
+    }
+
+
+
+});
