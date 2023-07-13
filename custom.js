fb7329e3b86e8063c581af28790c8865c5f32ca8
aecio <aeroimports2016@gmail.com>
2023-07-13 16:19:05 -0300

    novos

--- custom.js
+++ custom.js
@@ -8,6 +8,9 @@ $(document).ready(function () {
     if (urls.some(function(v) { return window.location.href.indexOf(v) > -1 })) {
 
        $('.ed_area_busca').remove()
+       $('.sw_titulo_pagina sw_ubuntu_light').text('MROSC - Parcerias')
+       $('.sw_area_links_navegacao').html(`<a href="/"><div class="sw_navegacao"><i class="swfa fas fa-home" aria-hidden="true"></i><span class="sw_lato">Principal</span></div></a>
+       <a href="${window.location.href}"><div class="sw_navegacao"><i class="swfa fas fa-home" aria-hidden="true"></i><span class="sw_lato">MROSC - Parcerias</span></div></a>`)
     }
 
 
